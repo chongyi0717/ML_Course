0b77cf01b5230ed4a362fb9c6532d60d4012d2c4
chongyi0717 <62092634+chongyi0717@users.noreply.github.com>
2020-05-14 03:09:56 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -41,7 +41,7 @@ def ml_loop(side: str):
 
         # 3.4 Send the instruction for this frame to the game process
         if not ball_served:
-            comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_RIGHT"})
+            comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_LEFT"})
             ball_served = True
         else:
             if side == "1P": 
